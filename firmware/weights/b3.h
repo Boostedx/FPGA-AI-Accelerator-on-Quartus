ae154b79ac8910f7bdc452dd6bc1dec753d970a0
RenzBoosted <renz.boosted@gmail.com>
2025-08-27 22:55:22 +0800

    Add files via upload

---
+++ firmware/weights/b3.h
@@ -0,0 +1,14 @@
+//Numpy array shape [16]
+//Min -0.101562500000
+//Max 0.281250000000
+//Number of zeros 0
+
+#ifndef B3_H_
+#define B3_H_
+
+#ifdef __INTELFPGA_COMPILER__
+hls_init_on_powerup
+#endif
+static const bias3_t b3[16] = {0.0156250, 0.0703125, 0.2578125, 0.2187500, 0.1875000, 0.0078125, -0.0312500, 0.0937500, 0.0468750, -0.1015625, 0.0781250, 0.2812500, 0.0703125, 0.2578125, -0.0625000, -0.0078125};
+
+#endif
